2daca6903a76239c60154865f4aa4c30c642c263
Gaby <gabbiebrabo@gmail.com>
2025-10-20 18:37:55 -0400

    jwt verify

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,18 +1,28 @@
+// app/src/api/index.ts
 import axios from "axios";
 import Cookies from "js-cookie";
+import { toast } from "sonner";
 
 const api = axios.create({
-  baseURL: "https://arduino-rirau-ipyra.onrender.com",
-  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("access_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      toast.error("Sessão expirada. Faça login novamente.");
+      Cookies.remove("access_token");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
